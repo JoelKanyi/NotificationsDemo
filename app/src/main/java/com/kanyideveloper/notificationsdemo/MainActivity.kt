d578845b5117ce5a32c07dc1ea4ac23a02acb7c1
JoelKanyi <joelkanyi98@gmail.com>
2021-02-21 13:33:13 +0300

    replying to a notification from the notification bar

--- app/src/main/java/com/kanyideveloper/notificationsdemo/MainActivity.kt
+++ app/src/main/java/com/kanyideveloper/notificationsdemo/MainActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.RemoteInput
 import com.kanyideveloper.notificationsdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private val CHANNEL_ID = "channelID"
     private val CHANNEL_NAME = "channelName"
     private val NOTIFICATION_ID = 0
+    private val KEY_REPLY = "key_reply"
     private var notificationManager: NotificationManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,55 +34,46 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showNotification(){
+    private fun showNotification() {
 
         //Opening the Notification in another activity
         val intent = Intent(this, SecondActivity::class.java)
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(
-                this,
-                0,
-                intent,
-                PendingIntent.FLAG_UPDATE_CURRENT
-        )
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        //Reply action
+        val remoteInput: RemoteInput = RemoteInput.Builder(KEY_REPLY).run {
+            setLabel("Insert your name here...")
+            build()
+        }
+        val replyAction: NotificationCompat.Action = NotificationCompat.Action.Builder(0, "REPLY", pendingIntent).addRemoteInput(remoteInput).build()
 
         //Action Buttons in Notification --- Home
         val intent2 = Intent(this, HomeActivity::class.java)
-        val pendingIntent2: PendingIntent = PendingIntent.getActivity(
-                this,
-                0,
-                intent2,
-                PendingIntent.FLAG_UPDATE_CURRENT
-        )
-        val action1 : NotificationCompat.Action = NotificationCompat.Action.Builder(
-                0,
-                "Home",
-                pendingIntent2).build()
+        val pendingIntent2: PendingIntent = PendingIntent.getActivity(this, 0, intent2, PendingIntent.FLAG_UPDATE_CURRENT)
+        val action1: NotificationCompat.Action = NotificationCompat.Action.Builder(0, "Home", pendingIntent2).build()
 
         //Action Buttons in Notification --- Setting
         val intent3 = Intent(this, SettingsActivity::class.java)
-        val pendingIntent3: PendingIntent = PendingIntent.getActivity(
-                this,
-                0,
-                intent3,
-                PendingIntent.FLAG_UPDATE_CURRENT
-        )
-        val action2 : NotificationCompat.Action = NotificationCompat.Action.Builder(
-                0,
-                "Settings",
-                pendingIntent3).build()
+        val pendingIntent3: PendingIntent = PendingIntent.getActivity(this, 0, intent3, PendingIntent.FLAG_UPDATE_CURRENT)
+        val action2: NotificationCompat.Action = NotificationCompat.Action.Builder(0, "Settings", pendingIntent3).build()
 
+        //Building the Notification
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("Notifications are Awesome")
-                .setContentText("Today I have to learn three sets of things, Notifications, WorkManager and Testing")
+                .setContentTitle("Jetpack Notifications are Awesome")
+                .setContentText("Today I have learnt Notifications in Android")
                 .setSmallIcon(R.drawable.ic_baseline_bug_report_24)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setContentIntent(pendingIntent)
+                //.setContentIntent(pendingIntent)
+
+                //Clicking on an action button takes you to its respective activity
                 .addAction(action1)
                 .addAction(action2)
+
+                //Click on this action enables you to reply directly on the notification bar
+                .addAction(replyAction)
                 .build()
 
         val notificationManager = NotificationManagerCompat.from(this)
-
         notificationManager.notify(NOTIFICATION_ID, notification)
     }
 
--- app/src/main/java/com/kanyideveloper/notificationsdemo/SecondActivity.kt
+++ app/src/main/java/com/kanyideveloper/notificationsdemo/SecondActivity.kt
@@ -1,11 +1,45 @@
 package com.kanyideveloper.notificationsdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.RemoteInput
+import com.kanyideveloper.notificationsdemo.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        receiveInput()
+    }
+
+    private fun receiveInput() {
+
+        val KEY_REPLY = "key_reply"
+        val CHANNEL_ID = "channelID"
+        val NOTIFICATION_ID = 0
+
+        val intent = this.intent
+        val remoteInput = RemoteInput.getResultsFromIntent(intent)
+        if (remoteInput != null) {
+            val inputString = remoteInput.getCharSequence(KEY_REPLY).toString()
+            binding.resultTextView.text = inputString
+
+            //Notifying the Notification that you have replied to it
+            val repliedNotification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_baseline_bug_report_24)
+                .setContentText("Reply Sent")
+                .build()
+
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(NOTIFICATION_ID, repliedNotification)
+        }
     }
 }
